d47130644396b7c801ae1ab8850e92cc071ff12c
Shashwat Dalal <spd16@ic.ac.uk>
2018-05-31 18:19:03 +0100

    [spd] added new routes

--- main.go
+++ main.go
@@ -7,5 +7,10 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./build")))
+	http.Handle("/matchmaking", http.FileServer(http.Dir("./build")))
+	http.Handle("/profile", http.FileServer(http.Dir("./build")))
+	http.Handle("/leaderboards", http.FileServer(http.Dir("./build")))
+	http.Handle("/teams", http.FileServer(http.Dir("./build")))
+	http.Handle("/chat", http.FileServer(http.Dir("./build")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
