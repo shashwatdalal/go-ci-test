5de4589b96c63eb54e3883ae76a3bf3d70192531
Marcel Kenlay <mkenlay16@gmail.com>
2018-05-31 12:53:33 +0100

    [mk] Created Jobs.js which reads from a json file

--- one-page-app/src/Chat.js
+++ one-page-app/src/Chat.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import FixtureList from './Chat/FixtureList';
+import Jobs from './Chat/Jobs';
 import './Stylesheets/Chat.css';
 
 class Chat extends Component {
   render() { return (
     <div class="chat_wrapper">
-      <div id="root" class="main"></div>
-      <div id="header" class="header"></div>
-      <div id="menu" class="sidemenu"></div>
+      <div id="chatlist" class="sidemenu"></div>
+      <div id="body" class="main"><Jobs/></div>
       <div id="fixtures" class="fixtures"><FixtureList/></div>
     </div>
   )}
