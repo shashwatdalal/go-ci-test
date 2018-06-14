3c9ae4d298fab263225338e26f8c21187a7a2bdc
Marcel Kenlay <mkenlay16@gmail.com>
2018-06-14 20:35:53 +0100

    [mk] Corrected switching of fixture list when chat is switched

--- src/Chat/FixtureList.js
+++ src/Chat/FixtureList.js
@@ -5,29 +5,40 @@ import './Stylesheets/FixtureList.css';
 var axios = require('axios');
 
 class FixtureList extends Component {
-    state = {
-        fixtures: []
-    }
+  state = {
+      fixtures: []
+  }
 
-    componentDidMount() {
-        var _this = this;
-        var req = "getPromotedFixtures?team=" + this.props.team_id
-        this.serverRequest = axios.get(req)
-            .then(function (result) {
-                _this.setState({fixtures: result.data});
-            })
-    }
+  componentDidMount() {
+    this.getPromotedFixtures()
+  }
 
-    render() {
-        return (
-            <div class="FixtureList">
-                <h1>Fixtures</h1>
-                {
-                    this.state.fixtures.map(fixture => (<FixtureCard key={`li-${fixture.AdID}`} data={fixture} team_id={this.props.team_id}/>))
-                }
-            </div>
-        );
+  getPromotedFixtures() {
+      var _this = this;
+      var req = "getPromotedFixtures?team=" + this.props.team_id
+      this.serverRequest = axios.get(req)
+          .then(function (result) {
+              _this.setState({fixtures: result.data});
+          })
+  }
+
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.team_id !== this.props.team_id) {
+     this.getPromotedFixtures()
     }
+  }
+
+  render() {
+      return (
+          <div class="FixtureList">
+              <h1>Fixtures</h1>
+              {
+                  this.state.fixtures.map(fixture => (<FixtureCard key={`li-${fixture.AdID}`} data={fixture} team_id={this.props.team_id}/>))
+              }
+          </div>
+      );
+  }
 }
 
 export default FixtureList;
