14eaa9494c94a6822a84efdd2749bc628d9426b9
Marcel Kenlay <mkenlay16@gmail.com>
2018-06-21 01:10:43 +0100

    [mk] fixture card css changes

--- src/Chat/FixtureCard.js
+++ src/Chat/FixtureCard.js
@@ -245,14 +245,14 @@ class FixtureCard extends Component {
                   <tr>
                     <td class={"votecell " + (this.state.upvoted ? 'vote-up-selected': 'vote-up')}
                       onClick={() => this.toggle_upvote()}>
-                      <h3 id="preftext">{"(" + this.state.upvotes + ") Yes"}</h3>
+                      <h3 class="preftext">{"(" + this.state.upvotes + ") Yes"}</h3>
                     </td>
 
                     <td class="separatorcell"></td>
 
                     <td class={"votecell " + (this.state.downvoted ? 'vote-down-selected': 'vote-down')}
                       onClick={() => this.toggle_downvote()}>
-                      <h3 id="preftext">{"(" + this.state.downvotes + ") No"}</h3>
+                      <h3 class="preftext">{"(" + this.state.downvotes + ") No"}</h3>
                     </td>
                   </tr>
                 </table>
